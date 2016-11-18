8fd0879447a348095e904f7776848aa8da24f9c3
pgaulier <paul.gaulier@free.fr>
2016-11-18 12:19:51 +0100

    update create.sql

---
+++ sql/create.sql
@@ -0,0 +1,153 @@
+-- =====================================================================
+--   Nom de la base   :  GESTION DES RELATIONS PARTENARIALES D'UNE ECOLE
+--   Nom de SGBD      :  Oracle Version 7.0
+--   Date de creation :  18/11/2016  11:38
+-- =====================================================================
+
+-- =====================================================================
+--   Table : ENTREPRISE
+-- =====================================================================
+create table ENTREPRISE
+(
+	NUMERO_ENTREPRISE          NUMBER(3)              not null,
+	NOM_ENTREPRISE		   CHAR(20)		  not null,
+	ADRESSE			   CHAR(50)			  ,
+	CODE_POSTAL		   NUMBER(10)		 	  ,
+	VILLE_SIEGE		   CHAR(20)		 	  ,
+	TELEPHONE		   NUMBER(20)			  ,
+	SITE_INTERNET		   CHAR(50)			  ,
+	NOMBRE_EMPLOYES		   NUMBER(5)			  ,
+	constraint pk_entreprise primary key (NUMERO_ENTREPRISE)
+);
+
+-- =====================================================================
+--   Table : DESCRIPTION_ENTREPRISE
+-- =====================================================================
+create table DESCRIPTION_ENTREPRISE
+(
+	NUMERO_ENTREPRISE	   NUMBER(3)              not null,
+	NUMERO_GENRE		   NUMBER(3)		  not null,
+	constraint pk_description_entreprise primary key (NUMERO_ENTREPRISE, NUMERO_GENRE)
+);
+
+-- =====================================================================
+--   Table : GENRE
+-- =====================================================================
+create table GENRE
+(
+	NUMERO_GENRE               NUMBER(3)              not null,
+	TYPE_GENRE 		   CHAR(20)			  ,
+	constraint pk_genre primary key (NUMERO_GENRE)
+);
+
+-- =====================================================================
+--   Table : TAXE
+-- =====================================================================
+create table TAXE
+(
+	NUMERO_TAXE		   NUMBER(3)              not null,
+	NUMERO_ENTREPRISE	   NUMBER(3)		  not null,
+	MONTANT			   NUMBER(3)		  not null,
+	ANNEE_VERSEMENT		   DATE				  ,
+	constraint pk_taxe primary key (NUMERO_TAXE)
+);
+
+-- =====================================================================
+--   Table : STAGE
+-- =====================================================================
+create table STAGE
+(
+	NUMERO_STAGE	           NUMBER(3)              not null,
+	NUMERO_ENTREPRISE 	   NUMBER(3)		  not null,
+	ID_SUPERVISEUR		   NUMBER(3) 		  not null,
+	SUJET			   CHAR(20)		          ,
+	DATE_DEBUT		   DATE				  ,
+	DATE_FIN		   DATE				  ,
+	constraint pk_stage primary key (NUMERO_STAGE)
+);
+
+-- =====================================================================
+--   Table : DESCRIPTION_STAGE
+-- =====================================================================
+create table DECRIPTION_STAGE
+(
+	NUMERO_TAG                 NUMBER(3)              not null,
+	NUMERO_STAGE		   NUMBER(3)		  not null,
+	constraint pk_description_stage primary key (NUMERO_TAG, NUMERO_STAGE)
+);
+
+-- =====================================================================
+--   Table : TAG
+-- =====================================================================
+create table TAG
+(
+	NUMERO_TAG                 NUMBER(3)              not null,
+	NUMERO_ENTREPRISE	   NUMBER(3)		  not null,
+	DESCRIPTION		   CHAR(20)		          ,
+	constraint pk_tag primary key (NUMERO_TAG)
+);
+
+-- =====================================================================
+--   Table : DESCRIPTION_STAGIAIRE
+-- =====================================================================
+create table DESCRIPTION_STAGIAIRE
+(
+	NUMERO_STAGE               NUMBER(3)              not null,
+	ID_STAGIAIRE		   NUMBER(3) 		  not null,
+	constraint pk_description_stagiaire primary key (NUMERO_STAGE, ID_STAGIAIRE)
+);
+
+-- =====================================================================
+--   Table : PERSONNE
+-- =====================================================================
+create table PERSONNE
+(
+	ID_PERSONNE                NUMBER(3)              not null,
+	NUMERO_ENTREPRISE	   NUMBER(3)		  not null,
+	NUMERO_ECOLE		   NUMBER(3)		  not null,
+	NOM			   CHAR(20)		  not null,
+	PRENOM			   CHAR(20)		          ,
+	ADRESSE			   CHAR(20)			  ,
+	TELEPHONE		   NUMBER(20)			  ,
+	MEL			   CHAR(40)			  ,
+	ANNEE_ETUDE		   NUMBER(1)			  ,
+	DEPARTEMENT		   CHAR(20)			  ,
+	PROMO			   CHAR(10)			  ,
+	FONCTION		   CHAR(20)			  ,
+	constraint pk_personne primary key (ID_PERSONNE)
+);
+
+-- ====================================================================
+--   Table : ECOLE
+-- ====================================================================
+create table ECOLE
+(
+	NUMERO_ECOLE               NUMBER(3)              not null,
+	NOM			   CHAR(20)		  not null,
+	TELEPHONE		   NUMBER(20)		          ,
+	ADRESSE			   CHAR(20)			  ,
+	constraint pk_ecole primary key (NUMERO_ECOLE)
+);
+
+-- ====================================================================
+--   Table : DESCRIPTION_CONTACT
+-- ====================================================================
+create table DESCRITPION_CONTACT
+(
+	NUMERO_EVENEMENT           NUMBER(3)              not null,
+	NUMERO_ECOLE		   NUMBER(3)		  not null,
+	NUMERO_ENTREPRISE	   NUMBER(3)		  not null,
+	ID_PERSONNE		   NUMBER(3)		  not null,
+	constraint pk_description_contact primary key (NUMERO_EVENEMENT, NUMERO_ECOLE, NUMERO_ENTREPRISE, ID_PERSONNE)
+);
+
+-- ====================================================================
+--   Table : EVENEMENT
+-- ====================================================================
+create table EVENEMENT
+(
+	NUMERO_EVENEMENT	   NUMBER(3)              not null,
+	NOM			   CHAR(20)		  not null,
+	DATE_EVENEMENT		   DATE			  not null,
+	constraint pk_evenement primary key (NUMERO_EVENEMENT)
+);
